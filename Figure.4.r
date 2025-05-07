80b1299064c72afd2c5f2eff2a2187d39c157a7c
WENPING-Z <97377392+WENPING-Z@users.noreply.github.com>
2025-05-07 20:40:26 +0800

    Add files via upload

---
+++ Figure.4.r
@@ -0,0 +1,195 @@
+
+# library
+library(dplyr)
+library(ggplot2)
+library(highcharter)
+library(ggsci)
+library(data.table)
+library(stringr)
+
+load("./Data/4.PloatData.RData")
+
+##### IS --------
+IS.shared.filter.df$ProcessedHabitat <- as.character(IS.shared.filter.df$ProcessedHabitat)
+
+across.df <- IS.shared.filter.df %>% filter(Shared.type == "crossHabitat") %>% 
+  group_by(ProcessedHabitat) %>% summarise(Count = n())
+
+across.df <- across.df %>% mutate(Habitat1 = sapply(strsplit(across.df$ProcessedHabitat, ".", fixed = T), "[[", 1),
+                                  Habitat2 = sapply(strsplit(across.df$ProcessedHabitat, ".", fixed = T), "[[", 2)) %>%
+  select(Habitat1, Habitat2, Count)
+
+
+colnames(across.df) <- c("from" , "to", "weight")
+across.df$to <- paste0(across.df$to, ".","1")
+
+#
+ordered_levels <- c("Tailings.1", "Farmland.1", "Forest.1", "Grassland.1", "Desert.1")
+across.df$to <- factor(across.df$to, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(to, levels = ordered_levels))
+
+ordered_levels <- c("Tailings", "Farmland", "Forest", "Grassland", "Desert")
+across.df$from <- factor(across.df$from, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(from, levels = ordered_levels))
+
+
+# 
+highchart( ) %>%
+  hc_chart(type = 'sankey', backgroundColor = 'white',borderRadius = 45) %>%
+  hc_add_series(data = across.df,
+                colorByPoint = TRUE, 
+                colors = c('#EA5C15','#addd8e','#f4d488','#2c7fb8','#7986cb')) %>%
+  hc_colorAxis(dataClassColor = "category") %>% 
+  hc_add_theme(hc_theme_gridlight())  #
+
+##### Phage -------
+across.df <- Phage.shared.filter.df %>% filter(Shared.type == "crossHabitat") %>% 
+  group_by(ProcessedHabitat) %>% summarise(Count = n()) %>%
+  mutate(Habitat1 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 1),
+         Habitat2 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 2)) %>%
+  select(Habitat1, Habitat2, Count)
+
+colnames(across.df) <- c("from" , "to", "weight")
+across.df$to <- paste0(across.df$to, ".","1")
+
+#
+ordered_levels <- c("Tailings.1", "Farmland.1", "Forest.1", "Grassland.1", "Desert.1")
+across.df$to <- factor(across.df$to, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(to, levels = ordered_levels))
+
+ordered_levels <- c("Tailings", "Farmland", "Forest", "Grassland", "Desert")
+across.df$from <- factor(across.df$from, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(from, levels = ordered_levels))
+
+#  
+highchart( ) %>%
+  hc_chart(type = 'sankey', backgroundColor = 'white',borderRadius = 45) %>%
+  hc_add_series(data = across.df,
+                colorByPoint = TRUE, 
+                colors = c('#EA5C15','#addd8e','#f4d488','#2c7fb8','#7986cb')) %>%
+  hc_colorAxis(dataClassColor = "category") %>% 
+  hc_add_theme(hc_theme_gridlight())  
+
+
+##### Plasmid ---------
+across.df <- Plasmid.shared.filter.df %>% filter(Shared.type == "crossHabitat") %>% 
+  group_by(ProcessedHabitat) %>% summarise(Count = n()) %>%
+  mutate(Habitat1 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 1),
+         Habitat2 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 2)) %>%
+  select(Habitat1, Habitat2, Count)
+
+colnames(across.df) <- c("from" , "to", "weight")
+across.df$to <- paste0(across.df$to, ".","1")
+
+#
+ordered_levels <- c("Tailings.1", "Farmland.1", "Forest.1", "Grassland.1", "Desert.1")
+across.df$to <- factor(across.df$to, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(to, levels = ordered_levels))
+
+ordered_levels <- c("Tailings", "Farmland", "Forest", "Grassland", "Desert")
+across.df$from <- factor(across.df$from, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(from, levels = ordered_levels))
+
+
+# 
+highchart( ) %>%
+  hc_chart(type = 'sankey', backgroundColor = 'white',borderRadius = 45) %>%
+  hc_add_series(data = across.df,
+                colorByPoint = TRUE, 
+                colors = c('#EA5C15','#addd8e','#f4d488','#2c7fb8','#7986cb')) %>%
+  hc_colorAxis(dataClassColor = "category") %>% 
+  hc_add_theme(hc_theme_gridlight())  
+
+##### ICE ---------
+
+across.df <- ICE.shared.filter.df %>% filter(Shared.type == "crossHabitat") %>% 
+  group_by(ProcessedHabitat) %>% summarise(Count = n()) %>%
+  mutate(Habitat1 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 1),
+         Habitat2 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 2)) %>%
+  select(Habitat1, Habitat2, Count)
+
+colnames(across.df) <- c("from" , "to", "weight")
+across.df$to <- paste0(across.df$to, ".","1")
+
+#
+ordered_levels <- c("Tailings.1", "Farmland.1", "Forest.1", "Grassland.1", "Desert.1")
+across.df$to <- factor(across.df$to, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(to, levels = ordered_levels))
+
+ordered_levels <- c("Tailings", "Farmland", "Forest", "Grassland", "Desert")
+across.df$from <- factor(across.df$from, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(from, levels = ordered_levels))
+
+
+#  
+highchart( ) %>%
+  hc_chart(type = 'sankey', backgroundColor = 'white',borderRadius = 45) %>%
+  hc_add_series(data = across.df,
+                colorByPoint = TRUE, 
+                colors = c('#EA5C15','#addd8e','#f4d488','#2c7fb8','#7986cb')) %>%
+  hc_colorAxis(dataClassColor = "category") %>% 
+  hc_add_theme(hc_theme_gridlight())
+
+
+##### Tn ----------
+across.df <- Tn.shared.filter.df %>% filter(Shared.type == "crossHabitat") %>% 
+  group_by(ProcessedHabitat) %>% summarise(Count = n()) %>%
+  mutate(Habitat1 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 1),
+         Habitat2 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 2)) %>%
+  select(Habitat1, Habitat2, Count)
+
+colnames(across.df) <- c("from" , "to", "weight")
+across.df$to <- paste0(across.df$to, ".","1")
+
+#
+ordered_levels <- c("Tailings.1", "Farmland.1", "Forest.1", "Grassland.1", "Desert.1")
+across.df$to <- factor(across.df$to, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(to, levels = ordered_levels))
+
+ordered_levels <- c("Tailings", "Farmland", "Forest", "Grassland", "Desert")
+across.df$from <- factor(across.df$from, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(from, levels = ordered_levels))
+
+
+#  
+highchart( ) %>%
+  hc_chart(type = 'sankey', backgroundColor = 'white',borderRadius = 45) %>%
+  hc_add_series(data = across.df,
+                colorByPoint = TRUE, 
+                colors = c('#EA5C15','#addd8e','#f4d488','#2c7fb8','#7986cb')) %>%
+  hc_colorAxis(dataClassColor = "category") %>% 
+  hc_add_theme(hc_theme_gridlight())  
+
+##### Integron ---------
+across.df <- Integron.shared.filter.df %>% filter(Shared.type == "crossHabitat") %>% 
+  group_by(ProcessedHabitat) %>% summarise(Count = n()) %>%
+  mutate(Habitat1 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 1),
+         Habitat2 = sapply(strsplit(ProcessedHabitat, ".", fixed = T), "[[", 2)) %>%
+  select(Habitat1, Habitat2, Count)
+
+colnames(across.df) <- c("from" , "to", "weight")
+across.df$to <- paste0(across.df$to, ".","1")
+
+#
+ordered_levels <- c("Tailings.1", "Farmland.1", "Forest.1", "Grassland.1", "Desert.1")
+across.df$to <- factor(across.df$to, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(to, levels = ordered_levels))
+
+ordered_levels <- c("Tailings", "Farmland", "Forest", "Grassland", "Desert")
+across.df$from <- factor(across.df$from, levels = ordered_levels)
+across.df <- across.df %>% arrange(factor(from, levels = ordered_levels))
+
+
+# 
+highchart( ) %>%
+  hc_chart(type = 'sankey', backgroundColor = 'white',borderRadius = 45) %>%
+  hc_add_series(data = across.df,
+                colorByPoint = TRUE, 
+                colors = c('#EA5C15','#addd8e','#f4d488','#2c7fb8','#7986cb')) %>%
+  hc_colorAxis(dataClassColor = "category") %>% 
+  hc_add_theme(hc_theme_gridlight())  
+
+
+
+
+
